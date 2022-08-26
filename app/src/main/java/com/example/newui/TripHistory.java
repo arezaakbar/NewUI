63288684535817a628a24e6cdcd048e63661c13e
arezaakbar <areza1068h@gmail.com>
2022-08-26 23:41:05 +0700

    New Fragment

---
+++ app/src/main/java/com/example/newui/TripHistory.java
@@ -0,0 +1,14 @@
+package com.example.newui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TripHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_history);
+    }
+}
