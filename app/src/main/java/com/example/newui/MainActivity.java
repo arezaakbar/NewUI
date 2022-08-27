06958951f266ae1ffc9a3057145d0d85419ad5af
arezaakbar <areza1068h@gmail.com>
2022-08-27 17:08:26 +0700

    Java Implemented

--- app/src/main/java/com/example/newui/MainActivity.java
+++ app/src/main/java/com/example/newui/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.newui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationMenu = findViewById(R.id.bottomNavigationMenu);
+        bottomNavigationMenu.setSelectedItemId(R.id.menumode);
+
+        bottomNavigationMenu.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.menumode:
+                        return true;
+                    case R.id.menuhistory:
+                        startActivity(new Intent(getApplicationContext(),TripHistory.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+
+                return false;
+            }
+        });
     }
 }
--- app/src/main/java/com/example/newui/TripHistory.java
+++ app/src/main/java/com/example/newui/TripHistory.java
@@ -1,14 +1,38 @@
 package com.example.newui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class TripHistory extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip_history);
+
+        bottomNavigationMenu.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.menumode:
+                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.menuhistory:
+                        return true;
+                }
+
+                return false;
+            }
+        });
     }
 }
